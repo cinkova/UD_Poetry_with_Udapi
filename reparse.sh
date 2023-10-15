e8969826e3a05f94e4f7c6f4e2d2c67feaff0696
Martin Popel <popel@ufal.mff.cuni.cz>
2023-10-15 02:45:22 +0200

    reparse the automatic version with UDPipe

---
+++ reparse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO automatic uses czech-pdt-ud-2.10-220711
+# but the newest is   czech-pdt-ud-2.12-230717
+
+mkdir -p raw reparse
+for f in automatic/*.conllu; do
+  fn=${f#automatic/}
+  fn=${fn%.conllu}
+  echo ======= Processing $f
+  cat $f | udapy write.Sentences | paste -s -d " " > raw/$fn.txt
+  # offline parsing with czech-pdt-ud-2.4-190531
+  # cat raw/$fn.txt | udapy -s read.Sentences udpipe.Cs resegment=1 util.Normalize sent_id_prefix=${fn%.conllu}_ > reparse/$fn.conllu
+  cat raw/$fn.txt | udapy -s read.Sentences udpipe.Cs model=czech-pdt-ud-2.10-220711 resegment=1 online=1 util.Normalize sent_id_prefix=${fn%.conllu}_ > reparse/$fn.conllu
+done
