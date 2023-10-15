2ce4f5b19f27e71dce15992fc6b0305a5b9785ad
Martin Popel <popel@ufal.mff.cuni.cz>
2023-10-16 00:00:15 +0200

    delete obsolete fix scripts

--- automatic/fix_ids.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-udapy read.Conllu files='!*.conllu' \
-  util.Eval tree='root.sent_id = root.document.meta["loaded_from"].replace(".conllu", "_") + root.sent_id' \
-  write.Conllu overwrite=1
