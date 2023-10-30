671f1b980d28b53c1dccf677e3a8284ad54acc48
Martin Popel <popel@ufal.mff.cuni.cz>
2023-10-30 16:09:52 +0100

    stats-edge-len.sh

--- stats-edge-len.sh
+++ stats-edge-len.sh
@@ -4,7 +4,7 @@
 
 if false; then
 cat all-resegmented.conllu | udapy -q util.MarkDiff gold_zone=gold attributes='deprel' ignore_parent=0 mark_attr=ParseErr \
- util.Mark mark_attr=PreGenNmod print_stats=1 node='(node < node.parent
+ util.Mark mark_attr=PreGenNmod print_stats=1 zones=gold node='(node < node.parent
   and node.feats["Case"]=="Gen"
   and node.udeprel == "nmod"
   and not any(c.udeprel=="case" for c in node.children)
@@ -13,6 +13,14 @@ cat all-resegmented.conllu | udapy -q util.MarkDiff gold_zone=gold attributes='d
  write.Conllu files=all-marked.conllu
 fi
 
+echo === GOLD ==== > generated/stats-edge-len.txt
 cat all-marked.conllu | udapy -q \
- util.See n=99 stats=edge node='node.misc["PreGenNmod"] and node.misc["ParseErr"]' \
- util.See n=99 stats=edge node='node.misc["PreGenNmod"]' > generated/stats-edge-len.txt
+ util.See zones=gold n=99 stats=edge node='node.misc["PreGenNmod"] and node.misc["ParseErr"]' \
+ util.See zones=gold n=99 stats=edge node='node.misc["PreGenNmod"]' \
+>> generated/stats-edge-len.txt
+
+echo -e "\n=== AUTO ====" >> generated/stats-edge-len.txt
+cat all-marked.conllu | udapy -q \
+ util.See zones=auto n=99 stats=edge node='node.misc["PreGenNmod"] and node.misc["ParseErr"]' \
+ util.See zones=auto n=99 stats=edge node='node.misc["PreGenNmod"]' \
+>> generated/stats-edge-len.txt
