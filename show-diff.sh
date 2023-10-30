417b528f6503720c528521ec5913ca8be5ca1a41
Martin Popel <popel@ufal.mff.cuni.cz>
2023-10-30 14:28:48 +0100

    update show-diff.sh and the generated files

--- show-diff.sh
+++ show-diff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-udapy -s read.Conllu files='!manual/*.conllu' zone=gold read.Conllu files='!reparse/*.conllu' zone=auto > all.conllu
+#udapy -s read.Conllu files='!manual/*.conllu' zone=gold read.Conllu files='!reparse/*.conllu' zone=auto > all.conllu
 cat all.conllu | udapy -s util.ResegmentGold > all-resegmented.conllu
-cat all-resegmented.conllu | udapy util.MarkDiff gold_zone=gold attributes='form,lemma,upos,deprel' write.TextModeTreesHtml > all-diff.html
+cat all-resegmented.conllu | udapy util.MarkDiff gold_zone=gold attributes='deprel' ignore_parent=0 write.TextModeTreesHtml > all-diff.html
