3bb01b99c628b37a9cb694d0994aedf44c66216f
Martin Popel <popel@ufal.mff.cuni.cz>
2023-11-02 05:57:16 +0100

    propagate validation fixes from UD dev branch

--- show-diff.sh
+++ show-diff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#udapy -s read.Conllu files='!manual/*.conllu' zone=gold read.Conllu files='!reparse/*.conllu' zone=auto > all.conllu
-cat all.conllu | udapy -s util.ResegmentGold > all-resegmented.conllu
-cat all-resegmented.conllu | udapy util.MarkDiff gold_zone=gold attributes='deprel' ignore_parent=0 write.TextModeTreesHtml > all-diff.html
+#udapy -s read.Conllu files='!manual/*.conllu' zone=gold read.Conllu files='!reparse/*.conllu' zone=auto > poetree-all.conllu
+cat poetree-all.conllu | udapy -s util.ResegmentGold > poetree-resegmented.conllu
+cat poetree-resegmented.conllu | udapy util.MarkDiff gold_zone=gold attributes='deprel' ignore_parent=0 write.TextModeTreesHtml > poetree-diff.html
